55388a853e4cb9580ef4abab3b9dd41e87b66460
Al-Braa <140897431+Al-Braa@users.noreply.github.com>
2025-02-27 23:36:23 +0200

    Remove <file/directory> from tracking

--- staticfiles/js/dark_mode.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.getElementById('dark-mode-toggle');
-    toggleButton.addEventListener('click', function () {
-        const body = document.body;
-        body.classList.toggle('dark-mode');
-        const darkModeEnabled = body.classList.contains('dark-mode');
-        document.cookie = `dark_mode=${darkModeEnabled}; path=/;`;
-    });
-});
